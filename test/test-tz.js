6e92df8bcd880077f7a718cb12354abaeea839de
al <albertincx@gmail.com>
2019-02-21 19:50:19 +0300

    updated google->teleport

--- test/test-tz.js
+++ test/test-tz.js
@@ -1,22 +1,20 @@
 var tz = require('../lib/tz');
 var path = require('path'),
-  assert = require('assert'),
-  inspect = require('util').inspect;
-var KEY = '';
+    assert = require('assert'),
+    inspect = require('util').inspect;
 var group = path.basename(__filename, '.js') + '/';
 [
-  // {data: '', expected: ''},
-  // {data: 'Moscow#', expected: 'Europe/Moscow'},
-  {data: 'Москва#', expected: 'Europe/Moscow'},
-  // {data: 'istanbul', expected: 'Europe/Istanbul'},
-  {data: '. جمهوری اسلا#می ایران', expected: 'Asia/Tehran'},
-  // {data: 'تهران', expected: 'Asia/Tehran'},
-  // {data: [55.755814,37.617635], timestamp: 123123, expected: 'Asia/Tehran'}
+    // {data: '', expected: ''},
+    {data: 'Москва#', expected: 'Europe/Moscow'},
+    // {data: 'istanbul', expected: 'Europe/Istanbul'},
+    {data: '. جمهوری اسلا#می ایران', expected: 'Asia/Tehran'},
+    // {data: 'تهران', expected: 'Asia/Tehran'},
+    {data: [55.755814,37.617635], timestamp: 123123, expected: 'Asia/Tehran'}
 
 ].forEach(async function (v) {
-  var result = await tz.getTimeZone(v.data, KEY),
-    msg = '[' + group + ']: output mismatch.\n'
-      + 'Saw: ' + inspect(result) + '\n'
-      + 'Expected: ' + inspect(v.expected);
-  assert.deepEqual(result, v.expected, msg);
+    var result = await tz.getTimeZone(v.data);
+    var msg = '[' + group + ']: output mismatch.\n'
+        + 'Saw: ' + inspect(result) + '\n'
+        + 'Expected: ' + inspect(v.expected);
+    assert.deepEqual(result, v.expected, msg);
 });
